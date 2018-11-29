7c7a8c83b1965eeb89ac00da05f11bec0406a9ef
hnagaty@Lenovo <hnagaty@hotmail.com>
2018-11-29 14:21:12 +0200

    Thursday night

--- clusterOnMRR.R
+++ clusterOnMRR.R
@@ -1,23 +1,25 @@
 # I should run the file "readGenericMRR.R" before this
 # the above script reads the names of complete MRR export files in a given folder
 
-library(readr)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
+# Load libraries ----------------------------------------------------------
+library(tidyverse)
 library(Hmisc)
 library(fastcluster)
-library(tibble)
-library(purrr)
-library(doParallel)
 library(dendextend)
 library(tictoc)
 library(dummies)
+library(sp)
+library(rgdal)
+library(spdplyr)
+library(tmap)
+library(RColorBrewer)
+library(viridisLite)
 
-
+# Parallel Processing -----------------------------------------------------
 registerDoParallel(cores=2) #multi-core like functionality
 
 
+# Functions & Indentifiers ------------------------------------------------
 pasteDir <- function(c) {
   if (nchar(c) == 2) {return (c)}
   else if (c=="BSPOWER") {return("DL")}
@@ -26,9 +28,10 @@ pasteDir <- function(c) {
 }
 
 dataDir<- "d:/data/mrr/2018Sep25/"
-dataDir <- "~/data/gmrr/"
+#dataDir <- "~/data/gmrr/"
 exportPath <- "D:/Optimisation/~InProgress/201806_GisFramework/export/"
-exportPath <- "~/Dropbox/Voda/GISWay/export/"
+#exportPath <- "~/Dropbox/Voda/GISWay/export/"
+geoPath <- "D:/Optimisation/~InProgress/201806_GisFramework/export"
 
 #Plotting functions
 kpiListBi1 <- c("PATHLOSS","RXLEV")
@@ -41,7 +44,7 @@ plotBiDirLine <- function(kpiV) {
   gmrrTidy %>% filter(kpi==kpiV) %>%
     ggplot(aes(x=Bin,y=Value,col=dir)) +
     geom_line(position = "dodge",size=1) +
-    facet_grid(cluster~.) +
+    facet_wrap(~cluster,ncol=3) +
     scale_y_continuous(labels = scales::percent) +
     labs(title=kpiV,subtitle="Distribution",x="Value",y="Percentage",color="Direction")
 }
@@ -50,7 +53,7 @@ plotBiDirBar <- function(kpiV) {
   gmrrTidy %>% filter(kpi==kpiV) %>%
     ggplot(aes(x=Bin,y=Value,fill=dir)) +
     geom_col(position = "dodge",col="grey") +
-    facet_grid(cluster~.) +
+    facet_wrap(~cluster,ncol=3) +
     scale_y_continuous(labels = scales::percent) +
     labs(title=kpiV,subtitle="Distribution",x="Value",y="Percentage",fill="Direction")
 }
@@ -63,19 +66,30 @@ plotUniDirLine <- function(kpiV) {
     labs(title=kpiV,subtitle="Distribution",x="Value",y="Percentage",color="Direction")
 }
 
+plotUniDirLineC <- function(kpiV) {
+  gmrrTidy %>% filter(kpi==kpiV) %>%
+    ggplot(aes(x=Bin,y=Value,col=cluster)) +
+    facet_wrap(~cluster,ncol=3) +
+    geom_line(position = "identity",size=1) +
+    scale_y_continuous(labels = scales::percent) +
+    labs(title=kpiV,subtitle="Distribution",x="Value",y="Percentage",color="Direction")
+}
+
 plotUniDirBar <- function(kpiV,taLimit) {
   gmrrTidy %>% filter(kpi==kpiV) %>%
     filter(!(kpi=="TAVAL" & Bin >= taLimit )) %>%
     mutate(Bin=as.factor(Bin)) %>%
     ggplot(aes(x=Bin,y=Value)) +
     geom_col(fill="orange") +
-    facet_grid(cluster~.) +
+    facet_wrap(~cluster,ncol=3) +
     scale_y_continuous(labels = scales::percent) +
     labs(title=kpiV,subtitle="Distribution",x="Value",y="Percentage",fill="Direction")
 }
 
 
 
+# Read MRR files ----------------------------------------------------------
+# mrrconf.df should be pre-defined. It's defined in "readGenericMRR.R"
 mrrFiles=paste0(mrrConf.df$File,".msmt")
 gmrr <- data.frame()
 for (m in mrrFiles) {
@@ -99,6 +113,8 @@ gmrr <- gmrr %>%
   filter(complete.cases(.))
 
 write_csv(gmrr,paste0(exportPath,"GsmMRR.csv"))
+
+# Reduce Features --------------------------------------------------------------
 gmrr <- read_csv(paste0(exportPath,"GsmMRR.csv"))
 nCols <- ncol(gmrr)
 
@@ -119,7 +135,7 @@ qtlsnames <- paste0("Q",qtls*100)
 
 #gmrr2 <-top_n(gmrr,100)
 
-tic("Tidy")
+tic("MRR Features")
 gmrrFeatures <-  gmrr %>%
   select(Cell,ChannelGroup,Band,`TrafficLevelCS(E)`,`RXQUALUL(0,0)`:`PATHLOSSDIFF(25,25)`) %>%
   rename(Traffic=`TrafficLevelCS(E)`) %>%  
@@ -143,17 +159,25 @@ toc()
 gmrrFeatures$Band <- as.factor(gmrrFeatures$Band)
 
 saveRDS(gmrrFeatures,file=paste0(exportPath,"gmrrFeatures.rds"))
+
+
+# Clustering --------------------------------------------------------------
 gmrrFeatures <- readRDS(file=paste0(exportPath,"gmrrFeatures.rds"))
-  
 
 data <- gmrrRatio %>%
   select(`TrafficLevelCS(E)`:`PATHLOSSDIFF(25,25)`)
 
-data <- select(gmrrFeatures,-Cell,-ChannelGroup,-Band)
+removeFeatures <- vars(Band,traffic)
+data <- gmrrFeatures %>%
+  filter(Band=="GSM900") %>%
+  select(-Cell,-ChannelGroup,-Band,-traffic)
+#data <- select(gmrrFeatures,-!!!removeFeatures) # I want to work this out
 #bandDummy <- dummy(gmrrFeatures$Band) #one hot encoding
 #rownames(bandDummy) <- gmrrFeatures$Cell #Not actually needed
 #data <- bind_cols(data,as.data.frame(bandDummy))
-labels <- select(gmrrFeatures,Cell,ChannelGroup)
+labels <- gmrrFeatures %>%
+  filter(Band=="GSM900") %>%
+  select(Cell,ChannelGroup)
 
 dataScaled <- scale(data)
 #mrr.dist <- dist(dataScaled, method = 'euclidean') #not needed as I use hclust,vector
@@ -161,26 +185,32 @@ dataScaled <- scale(data)
 tic("hclust")
 mrrHclust.fit <- hclust.vector(dataScaled,method="ward",metric="euclidean")
 toc()
-saveRDS(mrrHclust.fit,file=paste0(exportPath,"HClustFitReducedFeats_Nov25.rds"))
-mrrHclust.fit <- readRDS(file=paste0(exportPath,"HClustFitReducedFeats_Nov25.rds"))
+mrrHclust.fit$notes <- "V4: Removed band & traffic. GSM900 Only"
+saveRDS(mrrHclust.fit,file=paste0(exportPath,"HClustFitReducedFeats_V4_Nov26.rds"))
+
+mrrHclust.fit <- readRDS(file=paste0(exportPath,"HClustFitReducedFeats_V4_Nov26.rds"))
 
 # Dendogram plot
 k=2
-h=200
+h=150
 dendMrr <- as.dendrogram(mrrHclust.fit)
-dendColored <- color_branches(dendMrr, h = h)
+dendColored <- color_branches(dendMrr, h = h,groupLabels = TRUE)
 plot(dendColored,leaflab="none")
 
+# Cut the tree
 mrr.clusters <- as.factor(cutree(mrrHclust.fit, h = h))
 table(mrr.clusters)
 mrrClusters.df <- bind_cols(labels,data.frame(cluster=mrr.clusters))
-gmrrRatio <- inner_join(gmrrRatio,mrrClusters.df,by=c("Cell", "ChannelGroup"))
-table(gmrrRatio$cluster,gmrrRatio$Band) # ==> cluster 1 is mostly 1800, cluster is mostly 900
+gmrrClustered <- inner_join(gmrr,mrrClusters.df,by=c("Cell", "ChannelGroup"))
+table(gmrrClustered$cluster,gmrrClustered$Band) # ==> cluster 1 is mostly 1800, cluster is mostly 900
 
 
 
+# Plot the clusters -------------------------------------------------------
 # For plotting
-gmrrTidy <-  gmrrRatio %>%
+mrrHclust.fit$notes
+gmrrTidy <-  gmrrClustered %>%
+  mutate_at(vars(`RXQUALUL(0,0)`:`PATHLOSSDIFF(25,25)`),funs(. / NoReportsPassedFilter)) %>%
   group_by(cluster) %>%
   summarise_at(vars(`RXQUALUL(0,0)`:`PATHLOSSDIFF(25,25)`),mean) %>%
   gather("Measure","Value",`RXQUALUL(0,0)`:`PATHLOSSDIFF(25,25)`) %>%
@@ -196,5 +226,15 @@ gmrrTidy <-  gmrrRatio %>%
 map(kpiListBi1,plotBiDirLine)
 map(kpiListBi2,plotBiDirBar)
 map(kpiListUni1,plotUniDirLine)
-map(kpiListUni2,plotUniDirBar,8)
+map(kpiListUni1,plotUniDirLineC)
+map(kpiListUni2,plotUniDirBar,12)
 
+
+# Geographical Plots ------------------------------------------------------
+atollCells <- readOGR(geoPath,"atollCells")
+atollCells <- inner_join(atollCells,mrrClusters.df,by=c("CellName"="Cell"))
+tmap_mode("view")
+#myPal <- c("white","grey","grey","grey","green","grey","blue","red","grey")
+myPal <- brewer.pal(8,"Set1")
+tm_shape(atollCells) +
+  tm_polygons("cluster",palette=myPal) 
